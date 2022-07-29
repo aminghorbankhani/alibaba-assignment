f8e94d5218e181506f6c96c44701ee395d7310d1
aminghorbankhani <aminghorbankhani.ir@gmail.com>
2022-07-29 15:22:20 +0430

    refactor get country api call

--- src/components/DetailBorderColumnComponent.vue
+++ src/components/DetailBorderColumnComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link
-      :to="{ name: 'detail', params: { code: country.alpha2Code }}"
+      :to="{ name: 'detail', params: { code: country.alpha3Code }}"
       class="btn btn-sm shadow-sm px-3 mt-2 me-2 al-element al-text border-0"
   >
     {{ country.name }}
